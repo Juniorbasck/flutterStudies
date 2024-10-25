23f6db3f18263fbdc7c92b991f574b20df12c654
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-10-24 21:03:37 -0300

    declação de variaveis

---
+++ dart/tiposDados/meuapp/bin/meuapp.dart
@@ -0,0 +1,25 @@
+import 'package:meuapp/meuapp.dart' as meuapp;
+
+void main(List<String> arguments) {
+
+  String Texto = 'Mundo';
+  var Texto2 = 'Mundo';
+  String Texto3; 
+
+  int numero = 1;
+  int numero2 = 2;
+  int numero3;
+
+  print("Retorna veradeiro se o somente se esse inteiro for par");
+  print(numero.isEven);
+
+  print("Retorna veradeiro se o somente se esse inteiro for impar");
+  print(numero.isOdd);
+
+  List<String> lista = [];
+  lista.add('A');
+  var lista1 = []; /// tipo dinamico 
+  lista1.add(1);
+  lista1.add('A');
+
+}
