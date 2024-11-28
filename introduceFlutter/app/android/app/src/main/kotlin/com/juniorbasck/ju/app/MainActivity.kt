672c02fe0cfc0acdbe36eb0d8ddbf810f79a836b
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-11-28 16:18:12 -0300

    estrutura reps

--- introduceFlutter/app/android/app/src/main/kotlin/com/juniorbasck/ju/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.juniorbasck.ju.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ introduceFlutter/estruturasreps/bin/estruturasreps.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  var prova1 = 9;
+  var prova2 = 5;
+  var media = (prova1 + prova2) / 2;
+  print(media);
+  print(media = 7);
+  print(media < 7); 
+}
