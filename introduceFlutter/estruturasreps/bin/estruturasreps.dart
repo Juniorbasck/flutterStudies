0b602e09a4750508b06a8a16789fc8079721bba7
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-12-04 22:55:21 -0300

    if ternario

--- introduceFlutter/estruturasreps/bin/estruturasreps.dart
+++ introduceFlutter/estruturasreps/bin/estruturasreps.dart
@@ -2,7 +2,15 @@ void main(List<String> arguments) {
   var prova1 = 9;
   var prova2 = 5;
   var media = (prova1 + prova2) / 2;
-  print(media);
-  print(media = 7);
-  print(media < 7); 
+  if (media > 7) {
+    print('O aluno passou com nota $media');
+  } else if (media >= 5) {
+    print('O aluno ficou de recuperação com nota $media');
+  } else {
+    print('O aluno foi reprovado $media');
+  }
+
+  //if ternário
+  final resultado = media >= 7 ? 'Aprovado' : 'Reprovado';
+  print('O aluno foi $resultado');
 }
